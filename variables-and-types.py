9f754ac1d1e34e83a238e3783d12b720b22f89fc
wisyoong <wisyoong@gmail.com>
2020-07-13 09:42:48 +0800

    完成了 Variables and Types

---
+++ variables-and-types.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+x = 3
+f = 3.1415926
+name = "Python"
+
+print(x)
+print(f)
+print(name)
+
+combination = name + " " + name
+print(combination)
+
+sum = f + f
+print(sum)
+
+daysInYear =365
+daysInMonth = 30
+numberFiles = 5
+
+num_1 = 1
+num_2 = 2
+num_3 = 3
+print(num_1)
+print(num_2)
+print(num_3)
+
+print(64+32)
+x = 2
+y = 2
+print(x + y)
