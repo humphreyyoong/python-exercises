0c2c5b362a189a715e0d31ac861ab1f11543cfa7
wisyoong <wisyoong@gmail.com>
2020-07-22 21:32:47 +0800

    完成了 String replace

---
+++ string-replace.py
@@ -0,0 +1,21 @@
+s = "Hello World"
+print(s)
+s = s.replace("World","Universe")
+print(s)
+
+s = "Hello World World World"
+s = s.replace("World","Universe",1)
+print(s)
+
+"""
+Exercise
+Exercises below
+
+Try the replace program
+Can a string be replaced twice?
+Does replace only work with words or also phrases?
+"""
+
+s = "Python has a lot of functionality that comes out of the box."
+s = s.replace("comes out of the box","get out")
+print(s)
